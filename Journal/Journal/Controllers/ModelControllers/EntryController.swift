5c302f2d4554e88698332e20cbb4fb652fc06d53
Comet <travishalle@mac.com>
2021-04-19 18:36:18 -0400

    day 1 commit

---
+++ Journal/Journal/Controllers/ModelControllers/EntryController.swift
@@ -0,0 +1,8 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Travis Halle on 4/19/21.
+//
+
+import Foundation
---
+++ Journal/Journal/Models/Entry.swift
@@ -0,0 +1,8 @@
+//
+//  Entry.swift
+//  Journal
+//
+//  Created by Travis Halle on 4/19/21.
+//
+
+import Foundation
